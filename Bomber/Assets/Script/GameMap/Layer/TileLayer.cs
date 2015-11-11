3f7514ddf055f08e23e6536bfb9aaeafe8f845b4
ctpoyou <ctpoyou@naver.com>
2015-11-12 04:24:47 +0900

    prototype in-game core codes

---
+++ Bomber/Assets/Script/GameMap/Layer/TileLayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileLayer : Layer {
+
+	private const int NO_TILE = -1;
+
+	public GameObject[] tilePrefabs;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override Vector3 calcPosition( int xIndex, int yIndex )
+	{
+		return new Vector3 (xIndex * GameMapConst.BLOCK_SIZE, yIndex * GameMapConst.BLOCK_SIZE - GameMapConst.BLOCK_HEIGHT);
+	}
+
+	public override void parse( int[,] data )
+	{
+		
+		int i, j;
+		
+		for (i = 0; i < GameMapConst.MAP_INDEX_SIZE; ++ i) {
+			for (j = 0; j < GameMapConst.MAP_INDEX_SIZE; ++ j) {
+				if (data [i, j] != NO_TILE) {
+					Instantiate (tilePrefabs [data [i, j]],
+					             calcPosition (i, j),
+					             Quaternion.identity);
+				}
+			}
+		}
+	}
+}
---
+++ Bomber/Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private GameMap map;
+	// Use this for initialization
+	void Start () {
+
+		map = transform.GetComponentInChildren<GameMap>();
+		map.initGameMap ();
+
+		map.parseMap(
+			new int[,] {
+			{-1, -1, -1, -1, -1},
+			{-1, 0, -1, -1, -1},
+			{-1, -1, -1, -1, -1},
+			{-1, -1, -1, -1, -1},
+			{-1, -1, -1, -1, -1}
+		},
+		new int[,] {
+			{0, 0, 0, 0, 0},
+			{0, -1, -2, -2, 0},
+			{0, -2, 0, -2, 0},
+			{0, -2, -2, -2, 0},
+			{0, 0, 0, 0, 0}
+		}
+		);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
